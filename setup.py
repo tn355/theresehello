b62d5d7548ab72e138826005793eeefe82384a98
tn355 <40766556+tn355@users.noreply.github.com>
2018-07-02 10:23:13 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup( name='theresehello',
+       version='0.1',
+       description='This module greets the user.  ',
+       url='',                # usually a github URL
+       author='Therese Ngo',
+       author_email='NGO.THERESE@GMAIL.COM',
+       license='sbu',
+       packages=['theresehello'],
+       install_requires=[ ],
+       zip_safe=False )
